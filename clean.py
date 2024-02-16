b0e86542b137233ed2bc0c64a4e82a58e85da101
YAO SIJIA <23426829@life.hkbu.edu.hk>
2024-02-16 17:45:28 +0800

    data cleaning script

---
+++ clean.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+import sys
+def clean_data(input1, input2, output):
+
+    df1 = pd.read_csv(input1)
+    df2 = pd.read_csv(input2)
+    merged_df = pd.merge(df1, df2, left_on='respondent_id', right_on='id')
+    merged_df.dropna(inplace=True)
+    merged_df = merged_df[~merged_df['job'].str.contains('insurance|Insurance')]
+    merged_df.drop(columns='id', inplace=True)
+    merged_df.to_csv(output, index=False)
+
+
+if __name__ == "__main__":
+
+    input1 = sys.argv[1]
+    input2 = sys.argv[2]
+    output = sys.argv[3]
+    clean_data(input1, input2, output)
